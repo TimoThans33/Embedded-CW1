0c6506b1240874a9a6bc22537992a6139e419363
alexwillie <alex.willie.adolfsson@gmail.com>
2020-02-10 11:04:38 +0000

    I2C

--- Documents/simpletest.py
+++ Documents/simpletest.py
@@ -1,15 +1,26 @@
-# Simple demo of reading each analog input from the ADS1x15 and printing it to
-# the screen.
-# Author: Tony DiCola
-# License: Public Domain
+
 import time
 
-# Import the ADS1x15 module.
-import Adafruit_ADS1x15
+
+#import smbus
 from collections import deque
 
-# Create an ADS1115 ADC (16-bit) instance.
-adc = Adafruit_ADS1x15.ADS1115()
+# Create an instance
+#bus = smbus.SMBus(1)
+
+# Register and config values of ADS1115
+Addr = 0x48
+Conversion = 0x00
+Config = 0x01
+LowThreshold = 0x02
+HighThreshold = 0x03
+ModeSingle = 0x0100
+ModeContinuous = 0x0000
+OSConfig = 0x8000
+ConfigDR = 0x0040
+MuxOffset = 12
+Gain = 0x0200
+
 
 # Log data the last maxlen seconds
 Log = deque('',maxlen=60)
@@ -25,37 +36,32 @@ def ValueToAngle(value):
     # Calculate the angle from the sensor
     return round(abs(value-ValueStraight)/ValuePerAngle,0)
 
-# Or create an ADS1015 ADC (12-bit) instance.
-#adc = Adafruit_ADS1x15.ADS1015()
+channel = 0
+config = OSConfig
+print(hex(config))
+config |= ((channel+0x04) & 0x07) << MuxOffset
+print(hex(config))
+config |= ModeSingle
+print(hex(config))
+config |= ConfigDR
+print(hex(config))
 
-# Note you can change the I2C address from its default (0x48), and/or the I2C
-# bus by passing in these optional parameters:
-#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+bus.write_byte(Addr, config)
 
-# Choose a gain of 1 for reading voltages from 0 to 4.09V.
-# Or pick a different gain to change the range of voltages that are read:
-#  - 2/3 = +/-6.144V
-#  -   1 = +/-4.096V
-#  -   2 = +/-2.048V
-#  -   4 = +/-1.024V
-#  -   8 = +/-0.512V
-#  -  16 = +/-0.256V
-# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
-GAIN = 1
+data = bus.read_i2c_block_data(Addr, Conversion,2)
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-#print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(1)))
-print('-' * 37)
-# Main loop.
+intData = int.from_bytes(data,’big’)
+
+print(intData)
 
 
 
-while True:
-    # Read all the ADC channel values in a list.
-    value = adc.read_adc(0, gain=GAIN)
-    # print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*value))
-    # Pause for a second
+
+
+
+while False:
+
+
     print(value)
     angle = ValueToAngle(value)
     LogData(angle)
