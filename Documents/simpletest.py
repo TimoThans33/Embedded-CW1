4d2aca4b9c02da6ec90822256aea4b63dd1d43b6
alexwillie <alex.willie.adolfsson@gmail.com>
2020-02-10 17:25:47 +0000

    Acc I2C

--- Documents/simpletest.py
+++ Documents/simpletest.py
@@ -9,7 +9,7 @@ from collections import deque
 bus = smbus.SMBus(1)
 
 # Register and config values of ADS1115
-Addr = 0x48
+TempAddr = 0x48
 Conversion = 0x00
 Config = 0x01
 LowThreshold = 0x02
@@ -23,10 +23,22 @@ Gain = 0x0200
 CompDisable = 0x0003
 DataRate = 32
 
+# Accelerometer
+
+AccAddr = 0x1F
+AccID = 0xC7
+RegStatus = 0x00
+RegCtrlReg1 = 0x2A
+RegCtrlReg2 = 0x2B
+RegCtrlReg3 = 0x2C
+RegXYZData = 0x0E
+WhoAmI = 0x0D
+
+
 # Log data the last maxlen seconds
 Log = deque('',maxlen=60)
-ValueStraight = 31800
-ValueBend = 27000
+ValueStraight = 30550
+ValueBend = 26600
 ValuePerAngle = (ValueStraight-ValueBend)/90
 
 def LogData(value):
@@ -37,39 +49,53 @@ def ValueToAngle(value):
     # Calculate the angle from the sensor
     return round(abs(value-ValueStraight)/ValuePerAngle,0)
 
-def WriteI2C(Reg, Data):
+def WriteI2C(Addr, Reg, Data):
     # Write data to reg
     bus.write_i2c_block_data(Addr, Reg, Data)
 
-def ReadI2C(Reg, Len):
+def ReadI2C(Addr, Reg, Len):
     # Read byte from reg
     return bus.read_i2c_block_data(Addr, Reg, Len)
 
-def GetValueFromSensor(channel):
+def GetValueFromTempSensor():
     # Get the value of the sensor
-    reg = OSConfig
-    reg |= ((channel+0x04) & 0x07) << MuxOffset
-    reg |= Gain
-    reg |= ModeSingle
-    reg |= ConfigDR
-    reg |= CompDisable
-    WriteI2C(Config, [(reg >> 8) & 0xFF, reg & 0xFF])
+    # data size 15 byte
+    # Set OS
+    data = OSConfig
+    # Set Mux
+    data |= ((0x04) & 0x07) << MuxOffset
+    # Set Gain
+    data |= Gain
+    # Set Mode to single shot
+    data |= ModeSingle
+    # Set Data Rate
+    data |= ConfigDR
+    # Disable Comparator
+    data |= CompDisable
+    WriteI2C(TempAddr, Config, [(data >> 8) & 0xFF, data & 0xFF])
     # Wait for the data
     time.sleep(1/DataRate+0.0001)
 
-    bytes = ReadI2C(Conversion, 2)
+    bytes = ReadI2C(TempAddr, Conversion, 2)
 
     return int.from_bytes(bytes, "big")
 
 
+def SetModeAccSensor():
+    id =  ReadI2C(AccAddr, WhoAmI, 1)
+    print(id)
+    print(int.from_bytes(id, "big"))
 
 
 
-while True:
-    value = GetValueFromSensor(0)
-    print(value)
+#def GetValueFromAccSensor():
+
+SetModeAccSensor()    
+
+while False:
+    value = GetValueFromTempSensor()
     angle = ValueToAngle(value)
     LogData(angle)
-    print(angle)
+    print(LogData)
 
     time.sleep(1)
